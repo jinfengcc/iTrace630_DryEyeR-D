a6877db172e00fe9cfba7253a78c683c5b770cc9
Thanos Kontos <thanos.kontos@gmail.com>
2020-10-24 04:41:45 -0500

    Updated AppVersion

--- gui/AppVersion.cpp
+++ gui/AppVersion.cpp
@@ -1,11 +1,24 @@
 #include "stdafx.h"
 #include "AppVersion.h"
-#include <fmt/format.h>
-#include <gsl/gsl>
 
 namespace {
-  unsigned GetVVVVYMDD();
-}
+  constexpr unsigned GetVVVVYMDD(unsigned MAJOR, unsigned MINOR, unsigned YEAR, unsigned DAY)
+  {
+    unsigned VVVV = MAKEWORD(MINOR, MAJOR);
+    unsigned Y    = YEAR - 2015;
+    unsigned M    = DAY / 100;
+    unsigned DD   = DAY % 100;
+    unsigned YM   = (Y << 4) | M;
+    unsigned YMDD = MAKEWORD(DD, YM);
+
+    return MAKELONG(YMDD, VVVV);
+  }
+
+  constexpr unsigned GetVVVVYMDD()
+  {
+    return GetVVVVYMDD(VERSION_MAJOR, VERSION_MINOR, VERSION_YEAR, VERSION_DAY);
+  }
+} // namespace
 
 const unsigned SOFTWARE_VERSION_3 = 0x03000000;
 const unsigned SOFTWARE_VERSION_4 = 0x04000000;
@@ -26,38 +39,24 @@ CString FullSoftwareVersion()
   return s;
 }
 
+// Sanity checks
 namespace {
-  constexpr unsigned GetVVVVYMDD(unsigned MAJOR, unsigned MINOR, unsigned YEAR, unsigned DAY)
+  constexpr unsigned Str2Uint(const char *s)
   {
-    unsigned VVVV = MAKEWORD(MINOR, MAJOR);
-    unsigned Y    = YEAR - 2015;
-    unsigned M    = DAY / 100;
-    unsigned DD   = DAY % 100;
-    unsigned YM   = (Y << 4) | M;
-    unsigned YMDD = MAKEWORD(DD, YM);
-
-    return MAKELONG(YMDD, VVVV);
+    unsigned x = 0;
+    for (; *s != 0; ++s)
+      x = (x * 10) + (*s - '0');
+    return x;
   }
 
-  void VerifyStrings()
-  {
-    // clang-format off
-    static_assert(0x061e5a02 == GetVVVVYMDD(6, 30, 2020, 1002));
-    static_assert(0x07005a02 == GetVVVVYMDD(7,  0, 2020, 1002));
-    static_assert(0x070a5911 == GetVVVVYMDD(7, 10, 2020,  917));
+  // clang-format off
+  static_assert(0x07005a02 == GetVVVVYMDD(7,  0, 2020, 1002));
+  static_assert(0x070a5911 == GetVVVVYMDD(7, 10, 2020,  917));
 
-    Expects(fmt::format("{}"   , VERSION_MAJOR) == VERSION_MAJOR_STR);
-    Expects(fmt::format("{}"   , VERSION_MINOR) == VERSION_MINOR_STR);
-    Expects(fmt::format("{}"   , VERSION_YEAR ) == VERSION_YEAR_STR );
-    Expects(fmt::format("{:04}", VERSION_DAY  ) == VERSION_DAY_STR  );
+  static_assert(Str2Uint(VERSION_MAJOR_STR) == VERSION_MAJOR);
+  static_assert(Str2Uint(VERSION_MINOR_STR) == VERSION_MINOR);
+  static_assert(Str2Uint(VERSION_YEAR_STR ) == VERSION_YEAR );
+  static_assert(Str2Uint(VERSION_DAY_STR  ) == VERSION_DAY  );
+  // clang-format on
 
-    Expects(fmt::format("{:04}", 901  ) == "0901");
-    // clang-format on
-  }
-
-  unsigned GetVVVVYMDD()
-  {
-    VerifyStrings();
-    return GetVVVVYMDD(VERSION_MAJOR, VERSION_MINOR, VERSION_YEAR, VERSION_DAY);
-  }
 } // namespace
