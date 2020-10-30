3a19ea02957884bbf9a45d90408788fc9399781a
Miguel <maballeza11@gmail.com>
2020-10-30 10:58:21 -0500

    Update version number

--- gui/AppVersion.h
+++ gui/AppVersion.h
@@ -9,8 +9,8 @@
 #define VERSION_YEAR     2020
 #define VERSION_YEAR_STR "2020"
 
-#define VERSION_DAY     1029
-#define VERSION_DAY_STR "1029"
+#define VERSION_DAY     1030
+#define VERSION_DAY_STR "1030"
 
 #ifndef RC_INVOKED
 extern const unsigned SOFTWARE_VERSION_3;
