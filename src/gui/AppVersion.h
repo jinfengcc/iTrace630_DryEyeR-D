7497fbd1d297537829ddfd1abdf6f7874a5b8354
Miguel <maballeza11@gmail.com>
2021-02-26 10:42:08 -0600

    Version 2021.0226

--- src/gui/AppVersion.h
+++ src/gui/AppVersion.h
@@ -9,8 +9,8 @@
 #define VERSION_YEAR     2021
 #define VERSION_YEAR_STR "2021"
 
-#define VERSION_DAY     222
-#define VERSION_DAY_STR "0222"
+#define VERSION_DAY     226
+#define VERSION_DAY_STR "0226"
 
 #ifndef RC_INVOKED
 extern const unsigned SOFTWARE_VERSION_3;
