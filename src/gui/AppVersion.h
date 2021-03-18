90f0e7a198b3c3f1731a4c87b56d642fec320093
Thanos Kontos <thanos.kontos@gmail.com>
2021-03-18 07:39:16 -0500

    Ver 2021.0318

--- src/gui/AppVersion.h
+++ src/gui/AppVersion.h
@@ -9,8 +9,8 @@
 #define VERSION_YEAR     2021
 #define VERSION_YEAR_STR "2021"
 
-#define VERSION_DAY     317
-#define VERSION_DAY_STR "0317"
+#define VERSION_DAY     318
+#define VERSION_DAY_STR "0318"
 
 #ifndef RC_INVOKED
 extern const unsigned SOFTWARE_VERSION_3;
